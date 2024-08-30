e4b4f7bbfba4ea91a1172fd711d0675b8920cf85
aadi gupta <adityag6268@gmail.com>
2024-08-30 15:43:53 +0530

    first commit

---
+++ SIHproject/src/components/HeaderBanner/headerBanner.jsx
@@ -0,0 +1,385 @@
+import React from 'react';
+
+function HeaderBanner() {
+    return (
+        <>
+        <div className='bg-'>
+        <div className='flex px-8 mt-4 gap-10'>
+                <div className='flex gap-3 items-center justify-center'>
+                    <img className="h-16" alt="logo" src="image 8__0__1x.svg"/>
+                    <span className='text-lg font-semibold'>
+                        Alumni Association Platform  
+                    </span>
+                </div>
+                <div className='flex gap-4 mt-6 ml-72 text-sm font-semibold'>
+                    <span>
+                        Home
+                    </span>
+                    <span>
+                        My Donations
+                    </span>
+                    <span>
+                        Donation
+                    </span>
+                    <span>
+                        Events
+                    </span>
+                    <span>
+                        My Reports
+                    </span>
+                </div>
+                <div className='mt-4 ml-20'>
+                    <button className='bg-purple-950 brightness-110 text-slate-100 p-2 rounded-3xl px-5 text-sm'>Donate Now <span className='text-sm font-semibold'>!</span></button>
+                </div>
+            </div>
+
+        <div className='flex justify-center align-center'>
+            <div className='mt-16 flex-col items-center justify-center content-center mb-4 '>
+                        <div className='flex gap-2'>
+                            <span className='font-bold text-4xl text-black'>Alumni</span> <span className='font-bold text-4xl text-orange-600 brightness-110'>Donation Portal</span>
+                        </div>
+                        <div className='text-slate-500 text-xs pt-5 pl-5'>
+                            Strengthening connections and networking between alumni and <br/>students.
+                        </div>
+                    </div>
+
+        </div>
+            
+        <div className='flex align-center justify-center'>
+            <div className='flex align-center justify-center  pl-4 py-0 w-2/6  mt-4 bg-gray-100 rounded-3xl'>
+                <div className='relative flex justify-center items-center gap-3'>
+                    <img className='h-6 w-6' src="icons8-search-50.png"  alt="search"/>
+                    <div className='text-xs font-semibold pr-7'>
+                        Search by startup, project and more...
+                    </div>
+                    <button className='bg-purple-950 text-slate-100 p-2 rounded-3xl px-8 text-sm ml-10'>
+                        Search
+                    </button>
+                </div>
+            </div>
+        </div>
+        </div>
+
+
+        <div className='flex align-center justify-center py-24'>
+            <div className='flex-col align-center justify-center'>
+                <span className='text-2xl font-bold flex justify-center'>Impactful Alumni Stories !</span>
+                <div className='text-sm mt-0 text-slate-500'>
+                Contributing to the successful execution of startups and projects, driving significant growth.
+                </div>
+            </div>
+        </div>
+
+
+    3<div className='border-0 border-black border-solid flex flex-wrap gap-20 mx-20 items-center justify-center'>
+
+            <div className=' shadow-lg rounded-lg shadow-slate-300 w-72 p-2 pb-4'>
+
+                <div>
+                    <img className='w-72 rounded-lg' src="fastlife.png" alt='image'/>
+                </div>
+
+                <div className='flex-col justify-center items-center gap-4'>
+
+                    <div className='flex justify-between py-3'>
+                    <span>
+                        <div className='text-sm font-semibold '>Fastlife</div>
+                        <div className='text-xs text-gray-400 font-semibold'>Founder @nilay</div>
+                    </span>
+                    <span className='text-xs font-semibold text-gray-400 pr-2'>2024-08-08</span>
+                    </div>
+
+                    <p className='text-xs text-gray-400 leading-4 mb-4'>
+                    Ut enim ad minim veniam, quis nost exercitation ullamco laboris nisi ut allquip .
+                    </p>
+
+                    <div className='text-sm text-orange-700 brightness-150 font-semibold'>
+                        Entertainment
+                    </div>
+
+                    <button className=' absolute py-2 px-4 text-sm font-semibold bg-orange-500 brightness-110 text-white rounded-full mt-1 ml-20'> Donate Now!</button>
+
+                </div>
+            </div>
+
+            <div className=' shadow-lg rounded-lg shadow-slate-300 w-72 p-2 pb-4'>
+
+                <div>
+                    <img className='w-72 rounded-lg' src="leava.png" alt='image'/>
+                </div>
+
+                <div className='flex-col justify-center items-center gap-4'>
+
+                    <div className='flex justify-between py-3'>
+                    <span>
+                        <div className='text-sm font-semibold '>Fastlife</div>
+                        <div className='text-xs text-gray-400 font-semibold'>Founder @nilay</div>
+                    </span>
+                    <span className='text-xs font-semibold text-gray-400 pr-2'>2024-08-08</span>
+                    </div>
+
+                    <p className='text-xs text-gray-400 leading-4 mb-4'>
+                    Ut enim ad minim veniam, quis nost exercitation ullamco laboris nisi ut allquip .
+                    </p>
+
+                    <div className='text-sm text-orange-700 brightness-150 font-semibold'>
+                        Entertainment
+                    </div>
+
+                    <button className=' absolute py-2 px-4 text-sm font-semibold bg-orange-500 brightness-110 text-white rounded-full mt-2 ml-20'> Donate Now!</button>
+
+                </div>
+            </div>
+
+            <div className=' shadow-lg rounded-lg shadow-slate-300 w-72 p-2 pb-4'>
+
+                <div>
+                    <img className='w-72 rounded-lg' src="line.png" alt='image'/>
+                </div>
+
+                <div className='flex-col justify-center items-center gap-4'>
+
+                    <div className='flex justify-between py-3'>
+                    <span>
+                        <div className='text-sm font-semibold '>Fastlife</div>
+                        <div className='text-xs text-gray-400 font-semibold'>Founder @nilay</div>
+                    </span>
+                    <span className='text-xs font-semibold text-gray-400 pr-2'>2024-08-08</span>
+                    </div>
+
+                    <p className='text-xs text-gray-400 leading-4 mb-4'>
+                    Ut enim ad minim veniam, quis nost exercitation ullamco laboris nisi ut allquip .
+                    </p>
+
+                    <div className='text-sm text-orange-700 brightness-150 font-semibold'>
+                        Entertainment
+                    </div>
+
+                    <button className=' absolute py-2 px-4 text-sm font-semibold bg-orange-500 brightness-110 text-white rounded-full mt-2 ml-20'> Donate Now!</button>
+
+                </div>
+            </div>
+
+            <div className=' shadow-lg rounded-lg shadow-slate-300 w-72 p-2 pb-4'>
+
+                <div>
+                    <img className='w-72 rounded-lg' src="coffee.png" alt='image'/>
+                </div>
+
+                <div className='flex-col justify-center items-center gap-4'>
+
+                    <div className='flex justify-between py-3'>
+                    <span>
+                        <div className='text-sm font-semibold '>Fastlife</div>
+                        <div className='text-xs text-gray-400 font-semibold'>Founder @nilay</div>
+                    </span>
+                    <span className='text-xs font-semibold text-gray-400 pr-2'>2024-08-08</span>
+                    </div>
+
+                    <p className='text-xs text-gray-400 leading-4 mb-4'>
+                    Ut enim ad minim veniam, quis nost exercitation ullamco laboris nisi ut allquip .
+                    </p>
+
+                    <div className='text-sm text-orange-700 brightness-150 font-semibold'>
+                        Entertainment
+                    </div>
+
+                    <button className=' absolute py-2 px-4 text-sm font-semibold bg-orange-500 brightness-110 text-white rounded-full mt-2 ml-20'> Donate Now!</button>
+
+                </div>
+            </div>
+
+            <div className=' shadow-lg rounded-lg shadow-slate-300 w-72 p-2 pb-4'>
+
+                <div>
+                    <img className='w-72 rounded-lg' src="flash.png" alt='image'/>
+                </div>
+
+                <div className='flex-col justify-center items-center gap-4'>
+
+                    <div className='flex justify-between py-3'>
+                    <span>
+                        <div className='text-sm font-semibold '>Fastlife</div>
+                        <div className='text-xs text-gray-400 font-semibold'>Founder @nilay</div>
+                    </span>
+                    <span className='text-xs font-semibold text-gray-400 pr-2'>2024-08-08</span>
+                    </div>
+
+                    <p className='text-xs text-gray-400 leading-4 mb-4'>
+                    Ut enim ad minim veniam, quis nost exercitation ullamco laboris nisi ut allquip .
+                    </p>
+
+                    <div className='text-sm text-orange-700 brightness-150 font-semibold'>
+                        Entertainment
+                    </div>
+
+                    <button className=' absolute py-2 px-4 text-sm font-semibold bg-orange-500 brightness-110 text-white rounded-full mt-2 ml-20'> Donate Now!</button>
+
+                </div>
+            </div>
+
+            <div className=' shadow-lg rounded-lg shadow-slate-300 w-72 p-2 pb-4'>
+
+                <div>
+                    <img className='w-72 rounded-lg' src="chloe.png" alt='image'/>
+                </div>
+
+                <div className='flex-col justify-center items-center gap-4'>
+
+                    <div className='flex justify-between py-3'>
+                    <span>
+                        <div className='text-sm font-semibold '>Fastlife</div>
+                        <div className='text-xs text-gray-400 font-semibold'>Founder @nilay</div>
+                    </span>
+                    <span className='text-xs font-semibold text-gray-400 pr-2'>2024-08-08</span>
+                    </div>
+
+                    <p className='text-xs text-gray-400 leading-4 mb-4'>
+                    Ut enim ad minim veniam, quis nost exercitation ullamco laboris nisi ut allquip .
+                    </p>
+
+                    <div className='text-sm text-orange-700 brightness-150 font-semibold'>
+                        Entertainment
+                    </div>
+
+                    <button className=' absolute py-2 px-4 text-sm font-semibold bg-orange-500 brightness-110 text-white rounded-full mt-2 ml-20'> Donate Now!</button>
+
+                </div>
+            </div>
+
+        </div>
+
+
+        <div className='flex gap-24 pt-32 pl-32'>
+            <span>
+                <img className="h-3/4 pl-16" src="illustration-1.png" alt="illustration"/>
+            </span>
+            <span className='pl-20'> 
+                <div className='flex-col justify-center items-center'>
+                    <div className='text-4xl text-orange-500 brightness-110 font-bold pb-7'>
+                        Curious Minds Asks!
+                    </div>
+
+                    <div className='flex-col justify-center pb-6 items-center rounded-xl'>
+                        <span className='flex items-center  px-4 h-10 bg-gray-100 rounded-full w-96'>
+                            <img className='h-6 pr-4' src="idea-bulb-icon.svg"/>
+                            <span className='text-gray-800 text-sm pr-24'>How do donations help Alumni ? </span>
+                            <img className='h-2' src="line-angle-down-icon.svg"/>
+                        </span>
+                    </div>
+
+                    <div className='flex-col justify-center pb-6 items-center rounded-xl'>
+                        <span className='flex items-center  px-4 h-10 bg-gray-100 rounded-full w-96'>
+                            <img className='h-6 pr-4' src="idea-bulb-icon.svg"/>
+                            <span className='text-gray-800 text-sm pr-24'>How do donations help Alumni ? </span>
+                            <img className='h-2' src="line-angle-down-icon.svg"/>
+                        </span>
+                    </div>
+
+                    <div className='flex-col justify-center pb-6 items-center rounded-xl'>
+                        <span className='flex items-center  px-4 h-10 bg-gray-100 rounded-full w-96'>
+                            <img className='h-6 pr-4' src="idea-bulb-icon.svg"/>
+                            <span className='text-gray-800 text-sm pr-24'>How do donations help Alumni ? </span>
+                            <img className='h-2' src="line-angle-down-icon.svg"/>
+                        </span>
+                    </div>
+
+                    <div className='flex-col justify-center items-center rounded-xl'>
+                        <span className='flex items-center  px-4 h-10 bg-gray-100 rounded-full w-96'>
+                            <img className='h-6 pr-4' src="idea-bulb-icon.svg"/>
+                            <span className='text-gray-800 text-sm pr-24'>How do donations help Alumni ? </span>
+                            <img className='h-2' src="line-angle-down-icon.svg"/>
+                        </span>
+                    </div>
+
+                </div>
+            </span>
+        </div>
+
+
+        <div className='flex-col justify-center items-center pt-7'>
+            <div className='flex flex-col justify-center items-center content-center'>
+                    <div className='text-2xl font-bold pb-3'>
+                        Top Donors Appreciation!
+                    </div>
+                    <p className='text-sm mt-0 text-slate-500'>
+                        supporting new initiatives, fostering innovation, and creating lasting impact.  
+                    </p>
+            </div>   
+            <div className='flex flex-wrap justify-center items-center pt-12 gap-14'>
+
+                <div className='shadow-lg shadow-gray-300 rounded-lg p-3 w-62 '>
+                    <div className='text-xs text-gray-400 leading-4 pb-12 pt-4'>Donations made my research possible.<br/> Thank you!</div>
+                    <div className='flex items-center gap-4'>
+                        <span className='rounded-full'>
+                              <img className='h-7 rounded-full' src='pexels-jack-winbow-460463-1559486.jpg'/>
+                        </span>
+                        <span>
+                            <div className='text-sm font-semibold text-gray-800'>
+                                Finlay Kirk
+                            </div>
+                            <div className='text-xs mt-0 text-slate-500'>
+                                Web Developer
+                            </div>
+                        </span>
+                    </div>
+                </div>
+
+                <div className='shadow-lg shadow-gray-300 rounded-lg p-3 w-62 '>
+                    <div className='text-xs text-gray-400 leading-4 pb-12 pt-4'>Thank you to our generous alumni who <br/> have made significant contributions to <br/> our alumni association platform.</div>
+                    <div className='flex items-center gap-4'>
+                        <span className='rounded-full'>
+                              <img className='h-7 rounded-full' src='pexels-jack-winbow-460463-1559486.jpg'/>
+                        </span>
+                        <span>
+                            <div className='text-sm font-semibold text-gray-800'>
+                            Dannette P. Cervantes
+                            </div>
+                            <div className='text-xs mt-0 text-slate-500'>
+                                Web Design
+                            </div>
+                        </span>
+                    </div>
+                </div>
+
+                <div className='shadow-lg shadow-gray-300 rounded-lg p-3 w-62 '>
+                    <div className='text-xs text-gray-400 leading-4 pb-12 pt-4'>Your donations have played a vital role <br/>in supporting new initiatives, fostering<br/> innovation, and creating lasting impact</div>
+                    <div className='flex items-center gap-4'>
+                        <span className='rounded-full'>
+                              <img className='h-7 rounded-full' src='pexels-jack-winbow-460463-1559486.jpg'/>
+                        </span>
+                        <span>
+                            <div className='text-sm font-semibold text-gray-800'>
+                            Clara R. Altman
+                            </div>
+                            <div className='text-xs mt-0 text-slate-500'>
+                               UI/UX Design
+                            </div>
+                        </span>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+            <div className='flex justify-center items-center gap-12 pt-20 pl-10'>
+                <div className='flex flex-col justify-center items-center pr-10'>
+                    <div className='text-5xl text-orange-500 brightness-110 font-semibold pb-2'>5000</div>
+                    <div className='text-2xl font-bold'>Alumni Strong</div>
+                </div>
+
+                <div className='flex flex-col justify-center items-center pr-8'>
+                    <div className='text-5xl text-orange-500 brightness-110 font-semibold pb-2'>1 Million</div>
+                    <div className='text-2xl font-bold'>Dollars Raised</div>
+                </div>
+
+                <div className='flex flex-col justify-center items-center'>
+                    <div className='text-5xl text-orange-500 brightness-110 font-semibold pb-2'>100</div>
+                    <div className='text-2xl font-bold'>Scholarships Awarded</div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default HeaderBanner;
